8ecffdbe460841ec0f52a454cf0c6e8bff525964
killua <18763935133@163.com>
2021-12-05 18:18:50 +0800

    four

--- ch02-maven-web/src/main/java/com/bjpowernode/controller/HelloServlet.java
+++ ch02-maven-web/src/main/java/com/bjpowernode/controller/HelloServlet.java
@@ -9,7 +9,10 @@ public class HelloServlet {
     public static void main(String[] args) {
         System.out.println("6513");
         System.out.println("65136+984");
-        System.out.println("651351");
+        System.out.println("65135F1");
+        System.out.println("65135dA");
+        System.out.println("65135aSD1");
+
     }
 
 }
